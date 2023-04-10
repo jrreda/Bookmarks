10e32df16d0b24b44730daf5683c32048a756b69
Mahmoud Reda <mahmoudreda457@gmail.com>
2023-04-10 23:40:12 +0200

    add login/out

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -117,9 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Login/out
+LOGIN_REDIRECT_URL = "dashboard"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"

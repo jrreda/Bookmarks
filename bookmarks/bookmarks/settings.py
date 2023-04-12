48bbdff311630e81cbed086a6a3873147a136253
Mahmoud Reda <mahmoudreda457@gmail.com>
2023-04-12 17:38:13 +0200

    add django auth framework

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -129,3 +129,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "dashboard"
 LOGIN_URL = "login"
 LOGOUT_URL = "logout"
+
+
+# Emails
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
